dfb81d14ba23db84d9c7231f036229347702e69a
Rolf van de Krol <rvandekrol@swis.nl>
2025-06-13 10:58:04 +0200

    [#2] fix: make sure the created and updated templates don't blindly assume the presence of attributes

--- resources/views/entry-content/created.blade.php
+++ resources/views/entry-content/created.blade.php
@@ -1,3 +1,5 @@
 <x-filament-activitylog::entry-content-container :label="__('filament-activitylog::activitylog.events.created.label')">
-    <x-filament-activitylog::attributes-table :record="$record->subject" :newAttributes="$record->properties['attributes']" />
+    @if (isset($record->properties['attributes']))
+        <x-filament-activitylog::attributes-table :record="$record->subject" :newAttributes="$record->properties['attributes']" />
+    @endif
 </x-filament-activitylog::entry-content-container>
--- resources/views/entry-content/updated.blade.php
+++ resources/views/entry-content/updated.blade.php
@@ -1,3 +1,9 @@
 <x-filament-activitylog::entry-content-container :label="__('filament-activitylog::activitylog.events.updated.label')">
-    <x-filament-activitylog::attributes-table :record="$record->subject" :newAttributes="$record->properties['attributes']" :oldAttributes="$record->properties['old']" />
+    @if (isset($record->properties['attributes']))
+        @if (isset($record->properties['old']))
+            <x-filament-activitylog::attributes-table :record="$record->subject" :newAttributes="$record->properties['attributes']" :oldAttributes="$record->properties['old']" />
+        @else
+            <x-filament-activitylog::attributes-table :record="$record->subject" :newAttributes="$record->properties['attributes']" />
+        @endif
+    @endif
 </x-filament-activitylog::entry-content-container>
